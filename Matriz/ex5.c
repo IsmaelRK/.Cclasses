66fe469b8c88ced33cf11961a69b70e0cf6a2094
Ismael <ismael2021ab@gmail.com>
2023-06-27 23:39:14 -0300

    Feat: New Example

---
+++ Matriz/ex5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+main(){
+
+    int m[10][10], i, j, produto, entrada_temp;
+
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 10; j++){
+
+            printf("Digite um Numero: ");
+            scanf("%02d", &m[i][j]);
+        }
+    }
+
+
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 10; j++){
+
+            printf("%02d", m[i][j]); // %02d adiciona um 0 na frente dos números < 10 ou > -10
+        }
+        printf("\n");
+    }
+
+    printf("\n-------------------------------------");
+
+    for(i = 0; i < 10; i++){
+        for(j=0; j < 10; j++){
+
+            if(i > j){
+                produto = m[i][j] * 2;
+                printf("\n Produto: %d", produto);
+            }
+
+        }
+
+    }
+
+}
